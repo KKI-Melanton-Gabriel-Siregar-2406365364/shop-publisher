05e4a1c5bec30b193bb6fc9d4144410907a8d139
Twentism <melantongabriel@gmail.com>
2026-03-31 20:14:57 +0700

    Create Subscriber database and Subscriber repository struct skeleton.

---
+++ src/repository/subscriber.rs
@@ -0,0 +1,28 @@
+use dashmap::DashMap;
+use lazy_static::lazy_static;
+
+use crate::model::subscriber::Subscriber;
+
+// Singleton of Database
+lazy_static! {
+    static ref SUBSCRIBERS: DashMap<String, Vec<Subscriber>> = DashMap::new();
+}
+
+pub struct SubscriberRepository;
+
+impl SubscriberRepository {
+    pub fn add(product_type: String, subscriber: Subscriber) -> Subscriber {
+        let _ = (product_type, subscriber.clone());
+        subscriber
+    }
+
+    pub fn list_all(product_type: String) -> Vec<Subscriber> {
+        let _ = product_type;
+        vec![]
+    }
+
+    pub fn delete(product_type: String, url: String) -> bool {
+        let _ = (product_type, url);
+        false
+    }
+}
