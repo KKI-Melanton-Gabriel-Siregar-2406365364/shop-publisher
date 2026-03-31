28bd2fb08928a4d4370421df1bd7ed494079bc24
Twentism <melantongabriel@gmail.com>
2026-03-31 20:19:58 +0700

    Implement subscribe function in Notification controller.

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -1,15 +1,24 @@
+use rocket::http::Status;
+use rocket::response::status::Created;
 use rocket::serde::json::Json;
 
-use bambangshop::Result;
+use bambangshop::{Result, compose_error_response};
 use crate::model::subscriber::Subscriber;
 use crate::service::notification::NotificationService;
 
 #[post("/subscribe/<product_type>", data = "<subscriber>")]
-pub fn subscribe(product_type: String, subscriber: Json<Subscriber>) -> Result<Json<Subscriber>> {
-    NotificationService::subscribe(product_type, subscriber.into_inner()).map(Json::from)
+pub fn subscribe(product_type: String, subscriber: Json<Subscriber>) -> Result<Created<Json<Subscriber>>> {
+    match NotificationService::subscribe(product_type.clone(), subscriber.into_inner()) {
+        Ok(saved_subscriber) => Ok(Created::new(format!("/subscribe/{}", product_type)).body(Json::from(saved_subscriber))),
+        Err(error) => Err(error),
+    }
 }
 
 #[post("/unsubscribe/<product_type>?<url>")]
 pub fn unsubscribe(product_type: String, url: String) -> Result<Json<bool>> {
-    NotificationService::unsubscribe(product_type, url).map(Json::from)
+    let _ = (product_type, url);
+    Err(compose_error_response(
+        Status::NotImplemented,
+        String::from("Unsubscribe handler is not implemented yet."),
+    ))
 }
