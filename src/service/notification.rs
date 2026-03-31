c97d015e4f6c439bef7a4d16d19d9875bfa1a557
Twentism <melantongabriel@gmail.com>
2026-03-31 20:18:32 +0700

    Implement subscribe function in Notification service.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -3,15 +3,21 @@ use rocket::http::Status;
 use bambangshop::{Result, compose_error_response};
 use crate::model::notification::Notification;
 use crate::model::subscriber::Subscriber;
+use crate::repository::subscriber::SubscriberRepository;
 
 pub struct NotificationService;
 
 impl NotificationService {
-    pub fn subscribe(_product_type: String, _subscriber: Subscriber) -> Result<Subscriber> {
-        Err(compose_error_response(
-            Status::NotImplemented,
-            String::from("Subscribe is not implemented yet."),
-        ))
+    pub fn subscribe(product_type: String, subscriber: Subscriber) -> Result<Subscriber> {
+        if subscriber.url.is_empty() || subscriber.name.is_empty() {
+            return Err(compose_error_response(
+                Status::BadRequest,
+                String::from("Subscriber name and url must not be empty."),
+            ));
+        }
+
+        let saved_subscriber = SubscriberRepository::add(product_type, subscriber);
+        Ok(saved_subscriber)
     }
 
     pub fn unsubscribe(_product_type: String, _url: String) -> Result<bool> {
