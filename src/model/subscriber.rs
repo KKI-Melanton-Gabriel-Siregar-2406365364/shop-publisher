d3222838a30506067c817e79beb08895b6105be3
Twentism <melantongabriel@gmail.com>
2026-03-31 20:21:55 +0700

    Implement update method in Subscriber model to send notification HTTP requests.

--- src/model/subscriber.rs
+++ src/model/subscriber.rs
@@ -1,8 +1,31 @@
 use rocket::serde::{Deserialize, Serialize};
 
+use bambangshop::REQWEST_CLIENT;
+use crate::model::notification::Notification;
+
 #[derive(Debug, Clone, Deserialize, Serialize)]
 #[serde(crate = "rocket::serde")]
 pub struct Subscriber {
     pub name: String,
     pub url: String,
+}
+
+impl Subscriber {
+    pub fn update(&self, payload: Notification) -> bool {
+        let request_url = self.url.clone();
+        let runtime = rocket::tokio::runtime::Runtime::new();
+        if runtime.is_err() {
+            return false;
+        }
+
+        runtime.unwrap().block_on(async move {
+            REQWEST_CLIENT
+                .post(request_url)
+                .json(&payload)
+                .send()
+                .await
+                .map(|response| response.status().is_success())
+                .unwrap_or(false)
+        })
+    }
 }
